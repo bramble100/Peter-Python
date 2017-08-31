45fdc11bf2316401840f6ffa95ca4d6b879344cc
bramble100 <arnold.barna@gmail.com>
2017-08-31 15:12:13 +0200

    Test RegistryRowIsAddable unittesting is created

--- registry.py
+++ registry.py
@@ -62,23 +62,24 @@ class Registry(dict):
         if not row['Name']:
             self['Errors']['Errors found'] = True
             self['Errors']['Missing names'].add(row['ISIN'])
-            return True
 
         try:
-            (int(row['Months in Report']) if row['Months in Report'] != '' else 0) in (0, 3, 6, 9, 12)
+            months = int(row['Months in Report']) if row['Months in Report'] != '' else 0
+            if months not in (0, 3, 6, 9, 12):
+                raise ValueError
         except ValueError:
             self['Errors']['Errors found'] = True
             self['Errors']['Faulty months in report'].add(row['ISIN'])
-        finally:
-            return True
-
+            
         try:
-            datetime.datetime.strptime(str(row['Report Expiry Date']),
-                                       config['DEFAULT']['Date format'])
+            if row['Report Expiry Date']: datetime.datetime.strptime(row['Report Expiry Date'],
+                                                                     config['DEFAULT']['Date format'])
         except ValueError:
+            self['Errors']['Errors found'] = True
             self['Errors']['Faulty report expiry dates'].add(row['ISIN'])
-        finally:
-            return True
+
+        return True
+            
 
     def _registry_row_from_csv(self, row):
         '''Processes one stock data from a dict.'''
@@ -91,18 +92,112 @@ class Registry(dict):
                 'Stock Exchange Link': row['Stock Exchange Link']}
 
 def main():
+    '''Entry point for unit testing.'''
 
     test = TestRegistryRowIsAddable()
 
 class TestRegistryRowIsAddable(unittest.TestCase):
     '''Tests _registry_row_is_addable.'''
 
-    def test_missing_isin(self):
-        reg = Registry()
+    def setUp(self):
+        self.reg = Registry()
+
+    def test_missing_isin(self):        
         row = {'ISIN' : ''}
-        self.assertFalse(reg._registry_row_is_addable(row))
+        expected_errors = {'Errors found': True,
+                           'Number of missing ISINs': 1,
+                           'Faulty ISINs': set(),
+                           'Missing names': set(),
+                           'Faulty months in report': set(),
+                           'Faulty report expiry dates': set()}
+        self.assertFalse(self.reg._registry_row_is_addable(row))
+        self.assertEqual(self.reg['Errors'], expected_errors)
+
+    def test_faulty_isin(self):
         row = {'ISIN' : '12345678901'}
-        self.assertFalse(reg._registry_row_is_addable(row))
+        expected_errors = {'Errors found': True,
+                           'Number of missing ISINs': 0,
+                           'Faulty ISINs': set(['12345678901']),
+                           'Missing names': set(),
+                           'Faulty months in report': set(),
+                           'Faulty report expiry dates': set()}
+        self.assertFalse(self.reg._registry_row_is_addable(row))
+        self.assertEqual(self.reg['Errors'], expected_errors)
+
+    def test_missing_name(self):
+        row = {'ISIN' : '123456789012',
+               'Name' : '',
+               'Months in Report' : '',
+               'Report Expiry Date' : '2000.01.01'}
+        expected_errors = {'Errors found': True,
+                           'Number of missing ISINs': 0,
+                           'Faulty ISINs': set(),
+                           'Missing names': set(['123456789012']),
+                           'Faulty months in report': set(),
+                           'Faulty report expiry dates': set()}
+        self.assertTrue(self.reg._registry_row_is_addable(row))
+        self.assertEqual(self.reg['Errors'], expected_errors)
+
+    def test_unacceptable_months(self):
+        row = {'ISIN' : '123456789012',
+               'Name' : 'Company',
+               'Report Expiry Date' : '2000.01.01'}
+        expected_errors = {'Errors found': True,
+                           'Number of missing ISINs': 0,
+                           'Faulty ISINs': set(),
+                           'Missing names': set(),
+                           'Faulty months in report': set(['123456789012']),
+                           'Faulty report expiry dates': set()}
+        for months in ('X', '4'):
+            row['Months in Report'] = months
+            self.assertTrue(self.reg._registry_row_is_addable(row))
+            self.assertEqual(self.reg['Errors'], expected_errors)
+
+    def test_acceptable_months(self):
+
+        row = {'ISIN' : '123456789012',
+               'Name' : 'Company',
+               'Report Expiry Date' : '2000.01.01'}
+        expected_errors = {'Errors found': False,
+                           'Number of missing ISINs': 0,
+                           'Faulty ISINs': set(),
+                           'Missing names': set(),
+                           'Faulty months in report': set(),
+                           'Faulty report expiry dates': set()}
+        for months in ('', '3', '6', '9', '12'):
+            row['Months in Report'] = months
+            self.assertTrue(self.reg._registry_row_is_addable(row))
+            self.assertEqual(self.reg['Errors'], expected_errors)
+
+    def test_unacceptable_expiry_date(self):
+        row = {'ISIN' : '123456789012',
+               'Name' : 'Company',
+               'Months in Report' : '',
+               'Report Expiry Date' : 'x'}
+        expected_errors = {'Errors found': True,
+                           'Number of missing ISINs': 0,
+                           'Faulty ISINs': set(),
+                           'Missing names': set(),
+                           'Faulty months in report': set(),
+                           'Faulty report expiry dates': set(['123456789012'])}
+        self.assertTrue(self.reg._registry_row_is_addable(row))
+        self.assertEqual(self.reg['Errors'], expected_errors)
+
+    def test_acceptable_expiry_dates(self):
+
+        row = {'ISIN' : '123456789012',
+               'Name' : 'Company',
+               'Months in Report' : ''}
+        expected_errors = {'Errors found': False,
+                           'Number of missing ISINs': 0,
+                           'Faulty ISINs': set(),
+                           'Missing names': set(),
+                           'Faulty months in report': set(),
+                           'Faulty report expiry dates': set()}
+        for dates in ('', '2000.01.01'):
+            row['Report Expiry Date'] = dates
+            self.assertTrue(self.reg._registry_row_is_addable(row))
+            self.assertEqual(self.reg['Errors'], expected_errors)
 
 if __name__ == '__main__':
     main()
